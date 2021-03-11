3ca43792bb70c7e1f937bcafe79436afb150a9fc
Tom Fulton <tomfulton.mentor@gmail.com>
2021-03-10 22:49:35 -0500

    added dealer fields constructor get/set

--- src/com/games/blackjack/Dealer.java
+++ src/com/games/blackjack/Dealer.java
@@ -1,4 +1,17 @@
 package com.games.blackjack;
 
 public class Dealer {
+    private String name;
+
+    public Dealer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
