596cbd0a4e6f60f0cc1fb16d64438b6c8368ac10
Tom Fulton <tomfulton.mentor@gmail.com>
2021-03-11 15:20:52 -0500

    Added toString to Dealer

--- src/com/games/blackjack/Dealer.java
+++ src/com/games/blackjack/Dealer.java
@@ -14,4 +14,11 @@ public class Dealer {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Dealer{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
